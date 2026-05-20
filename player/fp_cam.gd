6979a26dfe83bd895d4a3298ce11c89e94096100
Christian Noll <christiannoll@gmx.net>
2026-05-20 19:01:30 +0200

    Fixed script fp_cam.gd

--- player/fp_cam.gd
+++ player/fp_cam.gd
@@ -3,7 +3,7 @@ extends Node3D
 var sensitivity = 0.2
 
 func _input(event: InputEvent) -> void:
-	if event == InputEventMouseMotion:
-		get_parent().rotate(deg_to_rad(-event.relative.x * sensitivity))
+	if event is InputEventMouseMotion:
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
 		rotate_x(deg_to_rad(-event.relative.y * sensitivity))
 		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90)) 
