cfae01a18a9050fff55cb87e43ffa9c53bd1b73a
Christian Noll <christiannoll@gmx.net>
2026-05-31 18:28:11 +0200

    Inverted player rotation

--- player/fp_cam.gd
+++ player/fp_cam.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
-		rotate_x(deg_to_rad(-event.relative.y * sensitivity))
+		get_parent().rotate_y(deg_to_rad(event.relative.x * sensitivity))
+		rotate_x(deg_to_rad(event.relative.y * sensitivity))
 		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90)) 
