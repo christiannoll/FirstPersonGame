b4532e6d100b3a36aa0058d0ed8673747ca6e33a
Christian Noll <christiannoll@gmx.net>
2026-08-20 19:17:40 +0200

    Added toggle func to the door script

--- general_scripts/door.gd
+++ general_scripts/door.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
+var opened = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func toggle_door():
+	if $AnimationPlayer.current_animation == null:
+		opened = !opened
+		if !opened:
+			$AnimationPlayer.play("close")
+		else:
+			$AnimationPlayer.pause("open")
