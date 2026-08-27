63e7a24f67e3f1615e2e571feb0ec337dd0f7931
Christian Noll <christiannoll@gmx.net>
2026-08-27 18:38:08 +0200

    Made the door script more readable

--- general_scripts/door.gd
+++ general_scripts/door.gd
@@ -5,7 +5,7 @@ var opened = false
 func toggle_door():
 	if $AnimationPlayer.current_animation != "open" and $AnimationPlayer.current_animation != "close":
 		opened = !opened
-		if !opened:
-			$AnimationPlayer.play("close")
 		if opened:
 			$AnimationPlayer.play("open")
+		else:
+			$AnimationPlayer.play("close")
